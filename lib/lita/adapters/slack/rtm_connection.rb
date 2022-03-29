88b1823b2c4b3e88ad84b04b3fbca6018ec03b63
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2022-03-29 13:52:38 -0500

    update to remove objects that were referenced with team_data.
    team_data from rtm.connect returns websocket and self (robot)

--- lib/lita/adapters/slack/rtm_connection.rb
+++ lib/lita/adapters/slack/rtm_connection.rb
@@ -3,10 +3,10 @@ require 'multi_json'
 
 require 'lita/adapters/slack/api'
 require 'lita/adapters/slack/event_loop'
-require 'lita/adapters/slack/im_mapping'
+# require 'lita/adapters/slack/im_mapping'
 require 'lita/adapters/slack/message_handler'
-require 'lita/adapters/slack/room_creator'
-require 'lita/adapters/slack/user_creator'
+# require 'lita/adapters/slack/room_creator'
+# require 'lita/adapters/slack/user_creator'
 
 module Lita
   module Adapters
@@ -24,17 +24,17 @@ module Lita
         def initialize(robot, config, team_data)
           @robot = robot
           @config = config
-          @im_mapping = IMMapping.new(API.new(config), team_data.ims)
+          # @im_mapping = IMMapping.new(API.new(config), team_data.ims)
           @websocket_url = team_data.websocket_url
           @robot_id = team_data.self.id
 
-          UserCreator.create_users(team_data.users, robot, robot_id)
-          RoomCreator.create_rooms(team_data.channels, robot)
+          # UserCreator.create_users(team_data.users, robot, robot_id)
+          # RoomCreator.create_rooms(team_data.channels, robot)
         end
 
-        def im_for(user_id)
-          im_mapping.im_for(user_id)
-        end
+        # def im_for(user_id)
+        #   im_mapping.im_for(user_id)
+        # end
 
         def run(queue = nil, options = {})
           EventLoop.run do
@@ -75,7 +75,7 @@ module Lita
         private
 
         attr_reader :config
-        attr_reader :im_mapping
+        # attr_reader :im_mapping
         attr_reader :robot
         attr_reader :robot_id
         attr_reader :websocket
